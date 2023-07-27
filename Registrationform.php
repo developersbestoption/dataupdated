61e91832c4dec366645817aa528dd3037214fe5b
developersbestoption <139047026+developersbestoption@users.noreply.github.com>
2023-07-27 15:01:55 +0530

    Update Registrationform.php

--- Registrationform.php
+++ Registrationform.php
@@ -3,6 +3,7 @@
 require 'conection.php';
 
 // Assuming you have a table named "register" with columns: id, sname, gender, dob, bloodgroup, placeofbirth, mothertongue, caste, religion, nationality, aadhar
+$id=$_POST['Rollno_txtbx'];
 $name = $_POST["name"];
 $gender = $_POST["Gender"];
 $dob = $_POST["dob"];
@@ -13,13 +14,21 @@ $caste = $_POST['caste'];
 $religion = $_POST['Religion'];
 $nationality = $_POST['Nationality'];
 $aadhar = $_POST['Aadhar'];
-
+$contact=$_POST['cnt_txtbx'];
+$mole=$_POST['mole_txtbx'];
+$email=$_POST['emailid_txtbx'];
+$scchall=$_POST['sscno_txtbx'];
+$sscboard=$_POST['sscbrddrpdwn'];
+$sscgpa=$_POST['sscper_txtbx'];
+$yearofpassing=$_POST['sscyeardrpdwn'];
+$sscschoolname=$_POST['schoolname_txtbx'];
+$passtype=$_POST['sscpassdrpdwn'];
 // Prepare the SQL statement
-$sql = "INSERT INTO register (sname, gender, dob, bloodgroup, placeofbirth, mothertongue, caste, religion, nationality, aadhar) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$sql = "INSERT INTO register (id,name,gender,dateofbirth,bloodgroup, placeofbirth, mothertongue, caste, religion, nationality, aadhar,contact,moles,emailid,sschallticket,sscboard,sscgpa,yearofpassing,sscschool,passtype) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 // Prepare and bind the statement
 $stmt = $connection->prepare($sql);
-$stmt->bind_param("ssssssssis", $name, $gender, $dob, $bloodgroup, $pob, $mothertongue, $caste, $religion, $nationality, $aadhar);
+$stmt->bind_param("issssssssisissssssss",$id, $name, $gender, $dob, $bloodgroup, $pob, $mothertongue, $caste, $religion, $nationality, $aadhar,$contact,$mole,$email,$scchall,$sscboard,$sscgpa,$yearofpassing,$sscschoolname,$passtype);
 
 // Execute the statement
 if ($stmt->execute()) {
