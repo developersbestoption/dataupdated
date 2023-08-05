3b796de831f6441e8b0827b2c3ac267d79dd9438
developersbestoption <139047026+developersbestoption@users.noreply.github.com>
2023-08-05 16:26:39 +0530

    Update basicdata.php

--- basicdata.php
+++ basicdata.php
@@ -1,11 +1,31 @@
 <?php
-// Include another PHP file
-require 'conection.php';
-$rollno = $_POST['Rollno_txtbx'];
-//$detail = $_GET['detail'];
-mysql_select_db($database) or die( "Unable to select database");
-$result = mysql_query("SELECT studentname, collegename,yoa,coursetype,course,branch,category,admno,doa FROM basicdata WHERE id =" .
-mysql_real_escape_string($_POST["rollno"]), $connection );
-$row = mysql_fetch_assoc($result);
-mysql_close($connection);
+if (isset($_GET['Rollno_txtbx '])) {
+    $primary_key = $_GET['Rollno_txtbx '];
+
+    // Assuming you have a database connection in connection.php
+    require 'conection.php';
+
+    // Assuming your table name is "your_table" and the primary key column is "id"
+    $sql = "SELECT * FROM basicdata WHERE rollno = ? LIMIT 1";
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param("s", $primary_key);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        // Redirect to page3.php with the fetched data as URL parameters
+      
+        header("Location: page3.php?id=" . $row['rollno'] . "&name=" . urlencode($row['name']) . "&collegename=" . urlencode($row['collegename'])."&studentname=" . urlencode($row['studentname'])."&yoa=" . urlencode($row['yoa'])."&coursetype=" . urlencode($row['coursetype'])."&course=" . urlencode($row['course'])."&branch	=" . urlencode($row['branch	'])."&category	=" . urlencode($row['category'])."&admno=" . urlencode($row['admno'])."&doa=" . urlencode($row['doa']));
+       
+        exit();
+    } else {
+        // Handle data not found
+        echo "Data not found.";
+    }
+    // Close the database connection
+    $stmt->close();
+    echo "Data not found.";
+    $connection->close();
+}
 ?>
